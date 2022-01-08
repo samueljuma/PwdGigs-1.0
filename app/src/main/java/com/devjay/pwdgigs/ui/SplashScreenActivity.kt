05d07a62002ee79c23fe86c2d7aafe75f9402264
samueljuma <samueljuma1024@gmail.com>
2022-01-08 08:45:55 +0300

    sign_in 101: design

--- app/src/main/java/com/devjay/pwdgigs/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/devjay/pwdgigs/ui/SplashScreenActivity.kt
@@ -22,7 +22,7 @@ class SplashScreenActivity : AppCompatActivity() {
                 val intent = Intent(this, SignInActivity::class.java)
                 startActivity(intent)
                 finish()
-            }, 3000
+            }, 1000
         )
 
 
