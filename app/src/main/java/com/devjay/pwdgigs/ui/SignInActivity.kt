5368b57f0606721548b90b342e8d30d6056cfda6
samueljuma <samueljuma1024@gmail.com>
2022-01-14 13:54:30 +0300

    home page design 101

--- app/src/main/java/com/devjay/pwdgigs/ui/SignInActivity.kt
+++ app/src/main/java/com/devjay/pwdgigs/ui/SignInActivity.kt
@@ -1,19 +1,37 @@
 package com.devjay.pwdgigs.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
 import com.devjay.pwdgigs.R
+import com.devjay.pwdgigs.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
+
+        binding.apply {
+            signInBtn.setOnClickListener {
+                val intent = Intent(this@SignInActivity, MainActivity::class.java)
+                startActivity(intent)
+//                finish()
+            }
+            signUpRedirect.setOnClickListener {
+                val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+                startActivity(intent)
+//                finish()
+            }
+        }
 
         
     }
--- app/src/main/java/com/devjay/pwdgigs/ui/SignUpActivity.kt
+++ app/src/main/java/com/devjay/pwdgigs/ui/SignUpActivity.kt
@@ -1,23 +1,40 @@
 package com.devjay.pwdgigs.ui
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.devjay.pwdgigs.R
+import com.devjay.pwdgigs.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
     @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
 
         // Removes status bar background
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
+
+        binding.apply {
+            signUpBtn.setOnClickListener {
+                val intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                startActivity(intent)
+
+            }
+            signInRedirect.setOnClickListener {
+                val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
+                startActivity(intent)
+            }
+        }
 
     }
 }
